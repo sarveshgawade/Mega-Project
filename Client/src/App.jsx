1a6b8bdaaff98cfc4e6eb963c96f3c49a10ae7ce
Sarvesh Sambhaji Gawade <sarveshgawadesg@gmail.com>
2024-02-01 18:42:24 +0530

    created footer

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,13 +1,17 @@
 import { Routes } from "react-router-dom"
+import Footer from "./components/Footer"
 
 
 function App() {
   
   return (
     <>
-      <Routes>
-        
-      </Routes>
+      {/* <Routes>
+
+
+      </Routes> */}
+
+      <Footer /> 
     </>
   )
 }
